129f10d4b64f569220cce16c0ad4cc4b55bdba52
Nazar0929 <166243366+Nazar0929@users.noreply.github.com>
2025-07-02 17:21:33 +0300

    push

--- js/scrol.js
+++ js/scrol.js
@@ -23,3 +23,23 @@ document.addEventListener("DOMContentLoaded", function () {
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
   });
 });
+
+
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-header-open]"),
+      closeModalBtn: document.querySelector("[data-modal-header-close]"),
+      modal: document.querySelector("[data-header-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+
+
+
